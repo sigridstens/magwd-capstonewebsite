7da98322fae94cc619e5a1e103b3503cfa248016
Sigrid Stensvold <sigridstens@gmail.com>
2020-11-14 18:14:33 -0600

    Deleted errors in js file

--- js/main.js
+++ js/main.js
@@ -1,18 +1,3 @@
-const navButton = document.getElementsByClassName('menu-icon')[0];
-const closeButton = document.getElementsByClassName('close-icon')[0];
-
-const headerNav = document.getElementsByClassName("header-nav")[0];
-
-navButton.addEventListener('click', function(headerNav) {
-headerNav.classList.toggle("nav-open");
-});
-
-closeButton.addEventListener('click', function(headerNav) {
-
-  headerNav.classList.toggle("nav-open");
-});
-
-
 $(function() {
   $("#main-content").mousewheel(function(event, delta) {
     this.scrollLeft -= (delta);
